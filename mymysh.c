852a60f22bd3185101c6a9a5d16c655c1182f300
HitoKage13 <38376447+HitoKage13@users.noreply.github.com>
2019-01-18 11:26:41 +1100

    v1.7

--- mymysh.c
+++ mymysh.c
@@ -1,7 +1,7 @@
 // mysh.c ... a small shell
 // Started by John Shepherd, September 2018
 // Completed by Jeremy Lim (z5209627), September/October 2018
-// Version 6 (01/10)
+// Version 7 (06/10)
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -31,7 +31,7 @@ void freeTokens(char **);
 char *findExecutable(char *, char **);
 int isExecutable(char *);
 void prompt(void);
-void execute(char **args, char **path, char **envp, int flag[]);
+void execute(char **args, char **path, char **envp);
 void printDir();
 
 
@@ -48,26 +48,25 @@ void printDir();
 // Set up enviroment and then run main loop
 // - read command, execute command, repeat
 
-int main(int argc, char *argv[], char *envp[])
-{
-   pid_t pid;   // pid of child process
-   int stat;    // return status of child
-   char **path; // array of directory names
-   int cmdNo;   // command number
-   int i;       // generic index
+int main(int argc, char *argv[], char *envp[]) {
+    pid_t pid;   // pid of child process
+    int stat;    // return status of child
+    char **path; // array of directory names
+    int cmdNo;   // command number
+    int i;       // generic index
 
-   // set up command PATH from environment variable
-   for (i = 0; envp[i] != NULL; i++) {
-      if (strncmp(envp[i], "PATH=", 5) == 0) break;
-   }
-   if (envp[i] == NULL)
-      path = tokenise("/bin:/usr/bin",":");
-   else
-      // &envp[i][5] skips over "PATH=" prefix
-      path = tokenise(&envp[i][5],":");
+    // set up command PATH from environment variable
+    for (i = 0; envp[i] != NULL; i++) {
+        if (strncmp(envp[i], "PATH=", 5) == 0) break;
+    }
+    if (envp[i] == NULL)
+        path = tokenise("/bin:/usr/bin",":");
+    else
+        // &envp[i][5] skips over "PATH=" prefix
+        path = tokenise(&envp[i][5],":");
 #ifdef DBUG
-   for (i = 0; path[i] != NULL;i++)
-      printf("path[%d] = %s\n",i,path[i]);
+    for (i = 0; path[i] != NULL;i++)
+        printf("path[%d] = %s\n",i,path[i]);
 #endif
 
     // initialise command history
@@ -75,27 +74,11 @@ int main(int argc, char *argv[], char *envp[])
     cmdNo = initCommandHistory();
     // main loop: print prompt, read line, execute command
 
-    int flag[1];
     char line[MAXLINE];
-    /* char *file = malloc(MAXLINE*sizeof(char));
-    file = getenv("HOME");
-    strcat(file,"/.mymysh_history");
-    FILE *fp = fopen(file,"a"); */
+
     prompt();
     while (fgets(line, MAXLINE, stdin) != NULL) {
-        flag[0] = 1;
         trim(line); // remove leading/trailing space
-        //   if empty command, ignore
-        // handle ! history substitution
-        // tokenise
-        // handle *?[~ filename expansion
-        // handle shell built-ins
-        // check for input/output redirections
-        // find executable using first token
-        // if none, then Command not found
-        // sort out any redirections
-        // run the command
-        // print prompt
 
         // Empty command
         if (!strcmp(line,"")) {
@@ -166,11 +149,9 @@ int main(int argc, char *argv[], char *envp[])
             cmdNo++;
             prompt();
             continue;
-        } else if (!strcmp(line,"debug")) {
-            debug();
         }
 
-        // Check for input/output redirections (WIP)
+        // Check for input/output redirections
         int j = 0;
         int in = 0;
         int out = 0;
@@ -191,14 +172,6 @@ int main(int argc, char *argv[], char *envp[])
             }
         }
 
-        // Unused > code
-        /* int input = open(args[j-1],O_RDONLY);
-        dup2(input,STDIN_FILENO);
-        close(input);
-        int output = open(args[j+1],O_WRONLY|O_CREAT,0666);
-        dup2(output,STDOUT_FILENO);
-        close(output); */
-
         // TODO
         // Code to implement mainloop goes here
         // Uses
@@ -214,10 +187,50 @@ int main(int argc, char *argv[], char *envp[])
             fprintf(stderr, "Failed to create child process\n");
             exit(1);
         } else if (pid > 0) {
+            // This is the parent process
+
+            // This is done here AND in execute(),
+            // to print the Executing command message
+            // It is a bit repetitive, but I believe it is necessary
+            // to do I/O redirections without messing up
+            // the other test cases.
+            if (findExecutable(args[0],path) != NULL) {
+                char* command = NULL;
+
+                if (args[0][0] == '/' || args[0][0] == '.') {
+                    if (isExecutable(args[0]))
+                        command = strdup(args[0]);
+                } else
+                for (i = 0; path[i] != NULL; i++) {
+                    char* filePath = malloc(strlen(path[i]) + strlen(args[0]) + 2);
+                    strcpy(filePath, path[i]);
+                    strcat(filePath, "/");
+                    strcat(filePath, args[0]);
+                    if (isExecutable(filePath)) {
+                        command = strdup(filePath);
+                        free(filePath);
+                        break;
+                    }
+                    free(filePath);
+                }
+                printf("Executing command: %s\n",command);
+                printf("--------------------\n");
+            } else {
+                printf("%s: Command not found\n",args[0]);
+            }
+
             wait(&stat);
+
+            if (findExecutable(args[0],path) != NULL) {
+                printf("--------------------\n");
+                printf("Return %d\n",WEXITSTATUS(stat));
+                addToCommandHistory(line,cmdNo);
+                cmdNo++;
+            }
             freeTokens(args);
         } else {
-            // Sort out redirections
+            // This is the child process
+            // Sorts out redirections
             if (in) {
                 int inFd = open(args[j+1],O_WRONLY|O_CREAT,0666);
                 if (args[j+2] == NULL) {
@@ -250,13 +263,7 @@ int main(int argc, char *argv[], char *envp[])
                 }
             }
 
-            execute(args,path,envp,flag);
-            if (findExecutable(args[0],path) != NULL) {
-                printf("--------------------\n");
-                printf("Return %d\n",WEXITSTATUS(stat));
-                addToCommandHistory(line,cmdNo);
-                cmdNo++;
-            }
+            execute(args,path,envp);
         }
 
         prompt();
@@ -270,8 +277,7 @@ int main(int argc, char *argv[], char *envp[])
 
 // fileNameExpand: expand any wildcards in command-line args
 // - returns a possibly larger set of tokens
-char **fileNameExpand(char **tokens)
-{
+char **fileNameExpand(char **tokens) {
     int i = 0;
     char *s = malloc((2*(MAXLINE+1)*sizeof(char)));
     while (tokens[i] != NULL) {
@@ -304,142 +310,127 @@ char **fileNameExpand(char **tokens)
 }
 
 // findExecutable: look for executable in PATH
-char *findExecutable(char *cmd, char **path)
-{
-      char executable[MAXLINE];
-      executable[0] = '\0';
-      if (cmd[0] == '/' || cmd[0] == '.') {
-         strcpy(executable, cmd);
-         if (!isExecutable(executable))
+char *findExecutable(char *cmd, char **path) {
+    char executable[MAXLINE];
+    executable[0] = '\0';
+    if (cmd[0] == '/' || cmd[0] == '.') {
+        strcpy(executable, cmd);
+        if (!isExecutable(executable))
             executable[0] = '\0';
-      }
-      else {
-         int i;
-         for (i = 0; path[i] != NULL; i++) {
+    }
+    else {
+        int i;
+        for (i = 0; path[i] != NULL; i++) {
             sprintf(executable, "%s/%s", path[i], cmd);
             if (isExecutable(executable)) break;
-         }
-         if (path[i] == NULL) executable[0] = '\0';
-      }
-      if (executable[0] == '\0')
-         return NULL;
-      else
-         return strdup(executable);
+        }
+        if (path[i] == NULL) executable[0] = '\0';
+    }
+    if (executable[0] == '\0')
+        return NULL;
+    else
+        return strdup(executable);
 }
 
 // isExecutable: check whether this process can execute a file
-int isExecutable(char *cmd)
-{
-   struct stat s;
-   // must be accessible
-   if (stat(cmd, &s) < 0)
-      return 0;
-   // must be a regular file
-   //if (!(s.st_mode & S_IFREG))
-   if (!S_ISREG(s.st_mode))
-      return 0;
-   // if it's owner executable by us, ok
-   if (s.st_uid == getuid() && s.st_mode & S_IXUSR)
-      return 1;
-   // if it's group executable by us, ok
-   if (s.st_gid == getgid() && s.st_mode & S_IXGRP)
-      return 1;
-   // if it's other executable by us, ok
-   if (s.st_mode & S_IXOTH)
-      return 1;
-   return 0;
+int isExecutable(char *cmd) {
+    struct stat s;
+    // must be accessible
+    if (stat(cmd, &s) < 0)
+        return 0;
+    // must be a regular file
+    //if (!(s.st_mode & S_IFREG))
+    if (!S_ISREG(s.st_mode))
+        return 0;
+    // if it's owner executable by us, ok
+    if (s.st_uid == getuid() && s.st_mode & S_IXUSR)
+        return 1;
+    // if it's group executable by us, ok
+    if (s.st_gid == getgid() && s.st_mode & S_IXGRP)
+        return 1;
+    // if it's other executable by us, ok
+    if (s.st_mode & S_IXOTH)
+        return 1;
+    return 0;
 }
 
 // tokenise: split a string around a set of separators
 // create an array of separate strings
 // final array element contains NULL
-char **tokenise(char *str, char *sep)
-{
-   // temp copy of string, because strtok() mangles it
-   char *tmp;
-   // count tokens
-   tmp = strdup(str);
-   int n = 0;
-   strtok(tmp, sep); n++;
-   while (strtok(NULL, sep) != NULL) n++;
-   free(tmp);
-   // allocate array for argv strings
-   char **strings = malloc((n+1)*sizeof(char *));
-   assert(strings != NULL);
-   // now tokenise and fill array
-   tmp = strdup(str);
-   char *next; int i = 0;
-   next = strtok(tmp, sep);
-   strings[i++] = strdup(next);
-   while ((next = strtok(NULL,sep)) != NULL)
-      strings[i++] = strdup(next);
-   strings[i] = NULL;
-   free(tmp);
-   return strings;
+char **tokenise(char *str, char *sep) {
+    // temp copy of string, because strtok() mangles it
+    char *tmp;
+    // count tokens
+    tmp = strdup(str);
+    int n = 0;
+    strtok(tmp, sep); n++;
+    while (strtok(NULL, sep) != NULL) n++;
+    free(tmp);
+    // allocate array for argv strings
+    char **strings = malloc((n+1)*sizeof(char *));
+    assert(strings != NULL);
+    // now tokenise and fill array
+    tmp = strdup(str);
+    char *next; int i = 0;
+    next = strtok(tmp, sep);
+    strings[i++] = strdup(next);
+    while ((next = strtok(NULL,sep)) != NULL)
+        strings[i++] = strdup(next);
+    strings[i] = NULL;
+    free(tmp);
+    return strings;
 }
 
 // freeTokens: free memory associated with array of tokens
 void freeTokens(char **toks)
 {
-   for (int i = 0; toks[i] != NULL; i++)
-      free(toks[i]);
-   free(toks);
+    for (int i = 0; toks[i] != NULL; i++)
+        free(toks[i]);
+    free(toks);
 }
 
 // trim: remove leading/trailing spaces from a string
-void trim(char *str)
-{
-   int first, last;
-   first = 0;
-   while (isspace(str[first])) first++;
-   last  = strlen(str)-1;
-   while (isspace(str[last])) last--;
-   int i, j = 0;
-   for (i = first; i <= last; i++) str[j++] = str[i];
-   str[j] = '\0';
+void trim(char *str) {
+    int first, last;
+    first = 0;
+    while (isspace(str[first])) first++;
+    last  = strlen(str)-1;
+    while (isspace(str[last])) last--;
+    int i, j = 0;
+    for (i = first; i <= last; i++) str[j++] = str[i];
+    str[j] = '\0';
 }
 
 // strContains: does the first string contain any char from 2nd string?
-int strContains(char *str, char *chars)
-{
-   for (char *s = str; *s != '\0'; s++) {
-      for (char *c = chars; *c != '\0'; c++) {
-         if (*s == *c) return 1;
-      }
-   }
-   return 0;
+int strContains(char *str, char *chars) {
+    for (char *s = str; *s != '\0'; s++) {
+        for (char *c = chars; *c != '\0'; c++) {
+            if (*s == *c) return 1;
+        }
+    }
+    return 0;
 }
 
 // prompt: print a shell prompt
 // done as a function to allow switching to $PS1
-void prompt(void)
-{
-   printf("mymysh$ ");
+void prompt(void) {
+    printf("mymysh$ ");
 }
 
 
 // execute: run a program, given command-line args, path and envp
-void execute(char **args, char **path, char **envp, int flag[])
-{
-    // TODO: implement the find-the-executable and execve() it code
-    //    args = tokenise the command line
-    // Used to denote if success or failed.
+void execute(char **args, char **path, char **envp) {
     char* command = NULL;
-//    if (args[0] starts with '/' or '.') {
     if (args[0][0] == '/' || args[0][0] == '.') {
-//    check if the file called args[0] is executable
         if (isExecutable(args[0]))
             command = strdup(args[0]);
-//    if so, use args[0] as the command
     } else {
         int i = 0;
         for (i = 0; path[i] != NULL; i++) {
-//       see if an executable file called "D/args[0]" exists
             char* filePath = malloc(strlen(path[i]) + strlen(args[0]) + 2);
             strcpy(filePath, path[i]);
             strcat(filePath, "/");
             strcat(filePath, args[0]);
-//       if it does, use that file name as the command
             if (isExecutable(filePath)) {
                 command = strdup(filePath);
                 free(filePath);
@@ -448,20 +439,13 @@ void execute(char **args, char **path, char **envp, int flag[])
             free(filePath);
         }
     }
-    if (command == NULL) {
-        printf("%s: Command not found\n",args[0]);
-        flag[0] = 0;
-    } else {
-//    print the full name of the command being executed
-        printf("Executing command: %s\n",command);
-        printf("--------------------\n");
-//    use execve() to attempt to run the command with args and envp
+
+    if (command != NULL) {
         if (execve(command,args,envp) == -1) {
             printf("Exec failed\n");
-//    if doesn't run, perror("Exec failed")
         }
     }
-// exit the child process
+
     exit(EXIT_SUCCESS);
 }
 
